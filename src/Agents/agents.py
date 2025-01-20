dab9286bda7faab3e63e80cda564b64ea1525dc8
glossner <john@glossner.org>
2025-01-20 15:15:05 -0500

    Initial agents, Dockerfile, and mnimal functionality

---
+++ src/Agents/agents.py
@@ -0,0 +1,48 @@
+import autogen
+import os
+from .interaction_agent import InteractionAgent
+from .nutrition_guide_agent import NutritionGuideAgent
+from .training_regiment_agent import TrainingRegimentAgent
+
+from src.Models.llm_config import gpt4_config
+from enum import Enum
+
+os.environ["AUTOGEN_USE_DOCKER"] = "False"
+
+###############################################
+# ChatGPT Model
+###############################################
+llm = gpt4_config
+
+#################################################
+# Define Agents
+#################################################
+
+interaction_agent = InteractionAgent(llm_config=llm)
+nutrition_guide_agent = NutritionGuideAgent(llm_config=llm)
+training_regiment_agent = TrainingRegimentAgent(llm_config=llm)
+
+
+class AgentKeys(Enum):    
+    INTERACTION_AGENT = 'interaction_agent'
+    NUTRITION_GUIDE_AGENT = 'nutrition_guide_agent'
+    TRAINING_REGIMENT_AGENT = 'training_regiment_agent'
+
+
+agents_dict = {
+    AgentKeys.INTERACTION_AGENT.value: interaction_agent,
+    AgentKeys.NUTRITION_GUIDE_AGENT.value: nutrition_guide_agent,
+    AgentKeys.TRAINING_REGIMENT_AGENT.value: training_regiment_agent,
+ }
+
+agents_dict_by_name = {
+    "InteractionAgent": interaction_agent,
+    "NutritionGuideAgent": nutrition_guide_agent,
+    "TrainingRegimentAgent": training_regiment_agent,    
+}
+
+avatars = {
+    interaction_agent.name: "✏️",                 # Pencil
+    nutrition_guide_agent.name: "🧑‍🎓",            # Person with graduation hat
+    training_regiment_agent.name:"💪", 
+}
---
+++ src/Agents/nutrition_guide_agent.py
@@ -0,0 +1,22 @@
+##################### Nutrition Guide #########################
+from typing import Dict
+from .conversable_agent import MyConversableAgent
+
+class NutritionGuideAgent(MyConversableAgent):
+    # description = ""
+    
+    system_message = """
+            You are the NutritionGuideAgent. 
+            Your role is to offer nutrition advice tailored to left-handed players. 
+            Create personalized meal plans, provide dietary tips, and monitor nutritional intake 
+            based on user input.            
+        """
+    def __init__(self, **kwargs):
+        super().__init__(
+                name="NutritionGuideAgent",
+                human_input_mode="NEVER",
+                system_message=kwargs.pop('system_message', self.system_message),
+                description=kwargs.pop('description',self.system_message),
+                **kwargs
+            )
+    

3bf7ee3fdb43ea53a22158e9f02021be405184df
Alex <121409950+AH0502@users.noreply.github.com>
2025-01-28 16:39:18 +0000

    Update conditioning_coach.py & agents.py

--- src/Agents/agents.py
+++ src/Agents/agents.py
@@ -2,7 +2,7 @@ import autogen
 import os
 from .interaction_agent import InteractionAgent
 from .nutrition_guide_agent import NutritionGuideAgent
-from .training_regiment_agent import TrainingRegimentAgent
+from .conditioning_coach import ConditioningCoachAgent
 from .training_report_agent import TrainingReportAgent
 
 from src.Models.llm_config import gpt4_config
@@ -21,34 +21,34 @@ llm = gpt4_config
 
 interaction_agent = InteractionAgent(llm_config=llm)
 nutrition_guide_agent = NutritionGuideAgent(llm_config=llm)
-training_regiment_agent = TrainingRegimentAgent(llm_config=llm)
+conditioning_coach_agent = ConditioningCoachAgent(llm_config=llm)
 training_report_agent = TrainingReportAgent(llm_config=llm)
 
 
 class AgentKeys(Enum):    
     INTERACTION_AGENT = 'interaction_agent'
     NUTRITION_GUIDE_AGENT = 'nutrition_guide_agent'
-    TRAINING_REGIMENT_AGENT = 'training_regiment_agent'
+    CONDITIONING_COACH_AGENT = 'conditioning_coach_agent'
     TRAINING_REPORT_AGENT = 'training_report_agent'
 
 
 agents_dict = {
     AgentKeys.INTERACTION_AGENT.value: interaction_agent,
     AgentKeys.NUTRITION_GUIDE_AGENT.value: nutrition_guide_agent,
-    AgentKeys.TRAINING_REGIMENT_AGENT.value: training_regiment_agent,
+    AgentKeys.CONDITIONING_COACH_AGENT.value: conditioning_coach_agent,
     AgentKeys.TRAINING_REPORT_AGENT.value: training_report_agent
  }
 
 agents_dict_by_name = {
     "InteractionAgent": interaction_agent,
     "NutritionGuideAgent": nutrition_guide_agent,
-    "TrainingRegimentAgent": training_regiment_agent, 
+    "ConditioningCoachAgent": conditioning_coach_agent, 
     "TrainingReportAgent": training_report_agent   
 }
 
 avatars = {
     interaction_agent.name: "✏️",                 # Pencil
     nutrition_guide_agent.name: "🧑‍🎓",            # Person with graduation hat
-    training_regiment_agent.name:"💪", 
+    conditioning_coach_agent.name:"💪", 
     training_report_agent.name: "🏋️‍♂️"
 }

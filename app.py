5c942acc6f8a0e76ce6df5d9ee05e3b51438337d
Alex <121409950+AH0502@users.noreply.github.com>
2025-02-20 21:51:39 +0000

    Create log crew - PTP.6
    Imported necessary agents, modified assessment crew inheritance, created
    RESTAPI functions for log crew.

--- app.py
+++ app.py
@@ -11,12 +11,27 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 import logging
 import uuid
+import crewai
+from crewai.knowledge.source.string_knowledge_source import StringKnowledgeSource
 
 from src.WebFunctions.run_full_assement import RunFullAssessmentCrew
 from src.WebFunctions.analyze_fitbit_data import AnalyzeFitbitDataCrew
 
 import src.Utils.utils as utils
 
+# Import Agents
+from src.Agents.agent_helpers import concatente_task_outputs
+from src.Agents.base_agent import BaseAgent
+from src.Agents.biomechanics_coach_agent import BiomechanicsCoachAgent
+from src.Agents.comprehensive_report_agent import ComprehensiveReportAgent
+from src.Agents.conditioning_coach_agent import ConditioningCoachAgent
+from src.Agents.exercise_database_agent import ExerciseDatabaseAgent
+from src.Agents.fitbit_agent import FitbitAgent
+from src.Agents.motivator_agent import MotivatorAgent
+from src.Agents.nutrition_agent import NutritionAgent
+from src.Agents.physiology_agent import PhysiologyAgent
+from src.Agents.position_coach_agent import PositionCoachAgent
+from src.Agents.psychology_agent import PsychologyAgent
 
 PORT = int(os.environ.get("PORT", 8000))  # Default to 8000 for local testing
 
@@ -37,7 +52,7 @@ app.add_middleware(
 # Store task results
 task_results = {}
 
-class AssessmentInput(BaseModel):
+class AssessmentInput(BaseAgent):
     file_path: str  # WordPress passes file URL or local path
 
 class AssessmentCrew:
@@ -89,7 +104,7 @@ class AssessmentCrew:
         result = crew.kickoff()       
         return result
 
-class UpdateInput(BaseModel):
+class UpdateInput(BaseAgent):
     file_path: str  # WordPress passes file URL or local path
 
 class UpdateCrew:
@@ -98,7 +113,7 @@ class UpdateCrew:
 
     def run(self, task_id: str):
         # Initialize agents with file input
-        analyst_agent = AnalystAgent()
+       # analyst_agent = AnalystAgent()
         conditioning_coach_agent = ConditioningCoachAgent()
         motivator_agent = MotivatorAgent()
         nutrition_agent = NutritionAgent()
@@ -106,7 +121,7 @@ class UpdateCrew:
         comprehensive_report_agent = ComprehensiveReportAgent()
 
         agents = [
-            analyst_agent,
+         #   analyst_agent,
             conditioning_coach_agent,
             motivator_agent,
             nutrition_agent,
@@ -115,7 +130,7 @@ class UpdateCrew:
         ]
 
         tasks = [
-            analyst_agent.analyze_data(),
+         #   analyst_agent.analyze_data(),
             conditioning_coach_agent.modify_training_program(),
             motivator_agent.motivate_athlete(),
             nutrition_agent.generate_meal_plan(),
@@ -135,7 +150,7 @@ class UpdateCrew:
         result = crew.kickoff()       
         return result
     
-class UpdateCrew:
+class LogCrew:
     def __init__(self, player_data: str):
         self.player_data = StringKnowledgeSource(content=player_data)
 
@@ -150,7 +165,7 @@ class UpdateCrew:
         ]
 
         tasks = [
-            conditioning_coach_agent.modify_training_program(),
+            conditioning_coach_agent.generate_report(),
             motivator_agent.motivate_athlete(),
         ]
 
@@ -160,14 +175,12 @@ class UpdateCrew:
             tasks=tasks,
             knowledge_sources=[self.player_data],
             process=crewai.Process.sequential,
-            verbose=False
+            verbose=True
         )
 
         result = crew.kickoff()       
-        return result
-
+        return concatente_task_outputs(result)
     
-
 
 
 @app.get("/")
@@ -232,5 +245,28 @@ def analyze_fitbit_data_run_and_store_result(task_id: str, input_text: str):
     task_results[task_id] = analyze_fitbit_data_result  # Store result for polling
 
 
+####################
+# Analyze Log Data
+####################
 
+@app.options("/log_training")  # Allow OPTIONS requests for CORS
+def preflight_check():
+    return {"message": "Preflight OK"}
+
+@app.post("/log_training")
+async def analyze_fitbit_data(
+    input_text: str = Body(..., media_type="text/plain"),
+    background_tasks: BackgroundTasks = BackgroundTasks()
+):
+    """Starts the Update as a background task and returns a task_id."""
+    task_id = str(uuid.uuid4())  # Generate a unique task ID
+    background_tasks.add_task(run_and_store_log_result, task_id, input_text)
+    return {"success": True, "task_id": task_id}
+
+
+def run_and_store_log_result(task_id: str, input_text: str):
+    """Runs the Update and stores the result for later retrieval."""
+    log_crew = LogCrew(input_text)
+    log_crew_result = log_crew.run(task_id)  # Runs synchronously in the background
+    task_results[task_id] = log_crew_result  # Store result for polling
 
